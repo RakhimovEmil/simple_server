06278db4482d14c2fc0720423f417dd8ac516292
RakhimovEmil <69158088+RakhimovEmil@users.noreply.github.com>
2020-12-22 17:49:22 +0300

    Update balance.py

--- balance/balance.py
+++ balance/balance.py
@@ -1,28 +1,33 @@
 from flask import Flask, request, make_response
 import requests
+import os 
 import json
 
 app = Flask(__name__)
+hosts = []
+
+def url(key):
+  host = hosts[len(key) % len(hosts)]
+  return f'http://{host}/storage/' + key
 
 @app.route("/<filename>", methods=['GET', 'PUT', 'DELETE'])
 def r(filename):
-  if len(filename) > 6:
-    url = 'http://instance1:8080/storage/' + filename
-  else:
-    url = 'http://instance2:8081/storage/' + filename
   if request.method == 'GET':
-    res = requests.get(url)
+    res = requests.get(url(filename))
     if res.status_code == 200:
       return make_response(res.text, res.status_code)
     else:
       return make_response("", res.status_code)
+    
   if request.method == 'PUT':
     req = request.get_json()
-    res = requests.put(url, json=req)
+    res = requests.put(url(filename), json=req)
     return make_response("", res.status_code)
+  
   if request.method == 'DELETE':
-    res = requests.delete(url, data=filename)
+    res = requests.delete(url(filename), data=filename)
     return make_response("", res.status_code)
 
 if __name__ == "__main__":
+    hosts = os.getenv('HOSTS').split(',')
     app.run(host='0.0.0.0', port=8082)
